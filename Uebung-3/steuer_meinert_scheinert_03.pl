5a526a6c5e34437df7e39461cda5fd6dd94cf7b0
mcd01 <4scheine@informatik.uni-hamburg.de>
2015-11-08 21:56:42 +0100

    Aufgabe 3 finished

--- Uebung-3/steuer_meinert_scheinert_03.pl
+++ Uebung-3/steuer_meinert_scheinert_03.pl
@@ -67,7 +67,43 @@ Y = 2 ;
 X = dokumente,
 Y = 4.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Aufgabe 3.1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+?- assert((namenliste_von_dir(DIRID, Ergebnis):- findall(B,file(_,DIRID,B,_,_,_),Ergebnis))).
+true.
+
+?- namenliste_von_dir(9,Ergebnis).
+Ergebnis = [in_the_summertime, i_am_so_romantic_tonight, ich_und_du_fuer_immer, paris].
+
+?- namenliste_von_dir(DIR,Ergebnis).
+Ergebnis = [in_the_summertime, i_am_so_romantic_tonight, ich_und_du_fuer_immer, paris, quartieranfrage, paris, dijon, die_bruecke_von_avignon, polterabend|...].
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Aufgabe 3.2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+?- assert((dirliste_von_dir(DIRID, Ergebnis):- findall(B,directory(_,B,DIRID,_,_), Ergebnis))).
+true.
+
+?- dirliste_von_dir(1,Ergebnis).
+Ergebnis = [bilder, musik, dokumente].
+
+?- dirliste_von_dir(DIR,Ergebnis).
+Ergebnis = [root, bilder, musik, dokumente, urlaub, hochzeit, kinder, klassik, pop|...].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Aufgabe 3.3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+?- assert((fileanzahl_von_dir(DIRID,Ergebnis):- length(Liste,Ergebnis), namenliste_von_dir(DIRID,Liste))).
+true.
+
+?- fileanzahl_von_dir(9,Anzahl).
+Anzahl = 4 
+
+?- fileanzahl_von_dir(DIR,Anzahl).
+Anzahl = 14 
 
