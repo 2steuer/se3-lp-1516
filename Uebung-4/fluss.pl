442ffd33a6b17bfad490d6d080f8f884f32099a0
mcd01 <4scheine@informatik.uni-hamburg.de>
2015-11-11 22:09:28 +0100

    Aufgabe 3.1 + 3.2 fertig

--- Uebung-4/fluss.pl
+++ Uebung-4/fluss.pl
@@ -1,44 +1,65 @@
-% liegt_stromaufwaerts_von(Fluss,Ort1,Ort2,Distanz)
+% liegt_liegt_stromaufwaerts_von_von(Fluss,Ort1,Ort2,Distanz)
 % ist wahr, wenn fuer zwei an einem Fluss liegende
 % Ortschaften die natuerliche Fliessrichtung des Wassers von
 % Ort1 zu Ort2 verlaeuft 
 % Distanz ist die Entfernung zwischen diesen Orten in Kilometern
 
-stromaufwaerts(moldau,praha,muendung_moldau,38).
+liegt_stromaufwaerts_von(moldau,praha,muendung_moldau,38).
+liegt_stromaufwaerts_von(elbe,muendung_moldau,usti,70). 
+liegt_stromaufwaerts_von(elbe,usti,dresden,93).
+liegt_stromaufwaerts_von(elbe,dresden,meissen,26).
+liegt_stromaufwaerts_von(elbe,meissen,torgau,73).
+liegt_stromaufwaerts_von(elbe,torgau,rosslau,102).
+liegt_stromaufwaerts_von(elbe,rosslau,muendung_saale,33).
+liegt_stromaufwaerts_von(elbe,muendung_saale,magdeburg,35).
+liegt_stromaufwaerts_von(elbe,magdeburg,tangermuende,63).
+liegt_stromaufwaerts_von(elbe,tangermuende,muendung_havel,34).
+liegt_stromaufwaerts_von(elbe,muendung_havel,wittenberge,31).
+liegt_stromaufwaerts_von(elbe,wittenberge,schnackenburg,21).
+liegt_stromaufwaerts_von(elbe,schnackenburg,geesthacht,111).
+liegt_stromaufwaerts_von(elbe,geesthacht,hamburg,22).
+liegt_stromaufwaerts_von(elbe,hamburg,muendung_elbe,125).
+liegt_stromaufwaerts_von(saale,calbe,muendung_saale,20).
+liegt_stromaufwaerts_von(saale,bernburg,calbe,16).
+liegt_stromaufwaerts_von(saale,halle,bernburg,57).
+liegt_stromaufwaerts_von(mulde,bitterfeld,rosslau,27).
+liegt_stromaufwaerts_von(mulde,wurzen,bitterfeld,47).
+liegt_stromaufwaerts_von(havel,havelberg,muendung_havel,3).
+liegt_stromaufwaerts_von(havel,rathenow,havelberg,42).
+liegt_stromaufwaerts_von(havel,brandenburg,rathenow,47).
+liegt_stromaufwaerts_von(havel,muendung_spree,brandenburg,55).
+liegt_stromaufwaerts_von(spree,berlin_mitte,muendung_spree,14).
+liegt_stromaufwaerts_von(oder,muendung_neisse,eisenhuettenstadt,11).
+liegt_stromaufwaerts_von(oder,eisenhuettenstadt,frankfurt_oder,33).
+liegt_stromaufwaerts_von(oder,frankfurt_oder,schwedt,111).
+liegt_stromaufwaerts_von(oder,schwedt,szczecin,42).
+liegt_stromaufwaerts_von(oder,szczecin,swinoujscie,61).
 
-stromaufwaerts(elbe,muendung_moldau,usti,70). 
-stromaufwaerts(elbe,usti,dresden,93).
-stromaufwaerts(elbe,dresden,meissen,26).
-stromaufwaerts(elbe,meissen,torgau,73).
-stromaufwaerts(elbe,torgau,rosslau,102).
-stromaufwaerts(elbe,rosslau,muendung_saale,33).
-stromaufwaerts(elbe,muendung_saale,magdeburg,35).
-stromaufwaerts(elbe,magdeburg,tangermuende,63).
-stromaufwaerts(elbe,tangermuende,muendung_havel,34).
-stromaufwaerts(elbe,muendung_havel,wittenberge,31).
-stromaufwaerts(elbe,wittenberge,schnackenburg,21).
-stromaufwaerts(elbe,schnackenburg,geesthacht,111).
-stromaufwaerts(elbe,geesthacht,hamburg,22).
-stromaufwaerts(elbe,hamburg,muendung_elbe,125).
+% ist_betroffen_von(Ort1,Ort2)
+% Ein Praedikat, das die wasserwirtschaftliche
+% Abhaeangigkeit der Ortschaft Ort1 von einer Schadstoffeinleitung an einem Ort Ort2 beschreibt.
 
-stromaufwaerts(saale,calbe,muendung_saale,20).
-stromaufwaerts(saale,bernburg,calbe,16).
-stromaufwaerts(saale,halle,bernburg,57).
+:- dynamic ist_betroffen_von/2.
 
-stromaufwaerts(mulde,bitterfeld,rosslau,27).
-stromaufwaerts(mulde,wurzen,bitterfeld,47).
+ist_betroffen_von(Ort1,Ort2):-
+                    liegt_stromaufwaerts_von(_,Ort2,Ort1,_).
 
-stromaufwaerts(havel,havelberg,muendung_havel,3).
-stromaufwaerts(havel,rathenow,havelberg,42).
-stromaufwaerts(havel,brandenburg,rathenow,47).
-stromaufwaerts(havel,muendung_spree,brandenburg,55).
+ist_betroffen_von(Ort1,Ort2):-
+                    liegt_stromaufwaerts_von(_,OrtX,Ort1,_),
+                    ist_betroffen_von(OrtX,Ort2).
 
-stromaufwaerts(spree,berlin_mitte,muendung_spree,14).
+% Praedikat fuer Aufgabe 3.2
+:- dynamic stoerfall_chemiewerk_bitterfeld/2.
+
+stoerfall_chemiewerk_bitterfeld(Liste):-
+                               findall(BetroffenerOrt,ist_betroffen_von(BetroffenerOrt,bitterfeld),Liste).
 
 
-stromaufwaerts(oder,muendung_neisse,eisenhuettenstadt,11).
-stromaufwaerts(oder,eisenhuettenstadt,frankfurt_oder,33).
-stromaufwaerts(oder,frankfurt_oder,schwedt,111).
-stromaufwaerts(oder,schwedt,szczecin,42).
-stromaufwaerts(oder,szczecin,swinoujscie,61).
+
+
+
+
+
+
+
 
--- Uebung-4/steuer_maier_scheinert_04.pl
+++ Uebung-4/steuer_maier_scheinert_04.pl
@@ -12,7 +12,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Aufgabe 2.1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Praedikat wurde in hauser.pl definiert.
+% Praedikate wurden in hauser.pl definiert.
 
 % Gibt wahr zurueck, wenn der Besitzer die Immobilie nach dem 31.12.2007 gekauft, und nicht mehr verkauft hat.
 
@@ -48,4 +48,63 @@ Vorbesitzer = [mueller, schulze, schneider, bund].
 ?- vorbesitzerSingle(3,mueller,Vorbesitzer).
 Vorbesitzer = schneider ;
 Vorbesitzer = schulze ;
+false.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Aufgabe 3.1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Praedikate wurden in fluss.pl definiert.
+
+% Terminierungssicher, da von einem Ort1 immer nur der unmittelbare Ort2 flussabwärts gezaehlt wird,
+% und ggf. alle Orte, die von diesem Ort2 betroffen sind.(Transitivitaet)
+% Da Fluesse bekanntlich nicht im Kreis verlaufen, terminiert unsere Loesung immer.
+
+?- ist_betroffen_von(Betroffener_Ort,muendung_neisse).
+Betroffener_Ort = eisenhuettenstadt ;
+Betroffener_Ort = frankfurt_oder ;
+Betroffener_Ort = schwedt ;
+Betroffener_Ort = szczecin ;
+Betroffener_Ort = swinoujscie ;
+false.
+
+?- ist_betroffen_von(Betroffener_Ort,berlin_mitte).
+Betroffener_Ort = muendung_spree ;
+Betroffener_Ort = wittenberge ;
+Betroffener_Ort = schnackenburg ;
+Betroffener_Ort = geesthacht ;
+Betroffener_Ort = hamburg ;
+Betroffener_Ort = muendung_elbe ;
+Betroffener_Ort = muendung_havel ;
+Betroffener_Ort = havelberg ;
+Betroffener_Ort = rathenow ;
+Betroffener_Ort = brandenburg ;
+false.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Aufgabe 3.2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Praedikate wurden in fluss.pl definiert.
+
+% Wir haben uns ueberlegt, dass rein theoretisch nur in jenen Ortschaften Trinkwasseralarm ausgeloest werden muss,
+% die flussabwaerts von dem Chemiewerk Bitterfeld liegen.
+% Ob dies in der Realitaet praktikabel und moralisch vertretbar ist, ist eine andere Frage.
+
+% Als Liste:
+
+?- stoerfall_chemiewerk_bitterfeld(Betroffene_Orte).
+Betroffene_Orte = [rosslau, muendung_saale, magdeburg, tangermuende, muendung_havel, wittenberge, schnackenburg, geesthacht, hamburg|...].
+
+% Alternativ als normale Aufzaehlung, um Korrektheit zu verdeutlichen:
+
+?- ist_betroffen_von(Betroffener_Ort,bitterfeld).
+Betroffener_Ort = rosslau ;
+Betroffener_Ort = muendung_saale ;
+Betroffener_Ort = magdeburg ;
+Betroffener_Ort = tangermuende ;
+Betroffener_Ort = muendung_havel ;
+Betroffener_Ort = wittenberge ;
+Betroffener_Ort = schnackenburg ;
+Betroffener_Ort = geesthacht ;
+Betroffener_Ort = hamburg ;
+Betroffener_Ort = muendung_elbe ;
 false.
