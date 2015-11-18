88a51d8928a7defa47ddd4df3c8918dad9fadaaf
mcd01 <4scheine@informatik.uni-hamburg.de>
2015-11-18 23:00:41 +0100

    Aufgabe 2.1 almost complete

--- Uebung-5/praedikate.pl
+++ Uebung-5/praedikate.pl
@@ -1,9 +1,41 @@
 :- dynamic nachfolger_peano/2.
-
+% Bestimmt den Nachfolger einer PEANO-Zahl.
+% Struktur: nachfolger_peano(Zahl,Nachfolgerzahl)
 nachfolger_peano(0,s(0)).
 nachfolger_peano(s(X),s(Y)) :- nachfolger_peano(X,Y). 
 
 :- dynamic vorgaenger_peano/2.
-
+% Bestimmt den Vorgaenger einer PEANO-Zahl.
+% Struktur: vorgaenger_peano(Zahl,Vorgaengerzahl)
 vorgaenger_peano(s(0),0) :- nachfolger_peano(0,s(0)).
 vorgaenger_peano(s(X),s(Y)):- nachfolger_peano(s(Y),s(X)).
+
+:- dynamic subtrahiere/3.
+% Realisiert die Subtraktion von PEANO-Zahlen.
+% Struktur: subtrahiere(Minuend,Subtrahend,Differenz)
+subtrahiere(X,0,X).
+subtrahiere(s(X),s(X),0).
+subtrahiere(X,s(Y),Z):- subtrahiere(X,Y,s(Z)).
+
+:- dynamic verdoppelt/2.
+% Prueft fuer zwei PEANO-Zahlen P1 und P2, ob P2 doppelt so gross ist wie P1.
+% Struktur: verdoppelt(Zahl,Zahlverdoppelt)
+verdoppelt(0,0).
+verdoppelt(s(Zahl),s(s(Zahlverdoppelt))):- verdoppelt(Zahl,Zahlverdoppelt).
+
+:- dynamic gte/2.
+gte(0,0).
+gte(s(X),0).
+gte(s(X),s(Y)):- gte(X,Y).
+
+:- dynamic modulo/3.
+% Struktur: modulo(Dividend,Divisor,Modulo)
+% Prueft, ob Dividend modulo Divisor = Modulo gilt.
+% Wir nutzen dabei als Hilfspraedikat das gte-Praedikat.
+modulo(s(0),s(0),0).
+modulo(Dividend,Divisor,Modulo):- gte(Dividend,Divisor), subtrahiere(Dividend,Divisor,Differenz), modulo(Differenz,Divisor,Modulo).
+modulo(Dividend,Divisor,Modulo):- not((gte(Dividend,Divisor))), Modulo = Dividend.
+
+:- dynamic peanoToInt/2.
+% Struktur: peanoToInt(Peano-Zahl,Integer-Zahl)
+% Wandelt eine Peano-Zahl in eine Integer-Zahl um.
--- Uebung-5/steuer_maier_scheinert_05.pl
+++ Uebung-5/steuer_maier_scheinert_05.pl
@@ -53,3 +53,74 @@ Praedikat: vorgaenger_von
         % ?- vorgaenger_peano(s(0),s(0)).
         % false.
         
+Praedikat: subtrahiere
+    % Positiv-Tests:
+        % ?- subtrahiere(0,0,0).
+        % true .
+
+        % ?- subtrahiere(s(0),0,s(0)).
+        % true .
+
+        % ?- subtrahiere(s(0),s(0),0).
+        % true .
+
+        % ?- subtrahiere(s(s(s(0))),Subtrahend,0).
+        % Subtrahend = s(s(s(0))) .
+
+        % ?- subtrahiere(s(s(s(0))),s(0),Differenz).
+        % Differenz = s(s(0)) .
+
+        % ?- subtrahiere(Minuend,s(0),s(s(s(0)))).
+        % Minuend = s(s(s(s(0)))).
+        
+    % Negativ-Tests:
+        % ?- subtrahiere(0,s(0),0).
+        % false.
+
+        % ?- subtrahiere(s(0),s(0),s(0)).
+        % false.
+        
+Praedikat: verdoppelt
+    % Positiv-Tests:
+        % ?- verdoppelt(0,0).
+        % true.
+
+        % ?- verdoppelt(s(s(0)),s(s(s(s(0))))).
+        % true.
+
+        % ?- verdoppelt(s(0),Zahlverdoppelt).
+        % Zahlverdoppelt = s(s(0)).
+
+        % ?- verdoppelt(Zahl,s(s(s(s(0))))).
+        % Zahl = s(s(0)).
+        
+    % Negativ-Tests:
+        % ?- verdoppelt(0,1).
+        % false.
+
+        % ?- verdoppelt(s(s(0)),s(s(0))).
+        % false.
+
+        % ?- verdoppelt(s(s(0)),s(0)).
+        % false.
+        
+Praedikat: modulo
+    % Positiv-Tests:
+        % ?- modulo(s(s(s(0))),s(0),0).
+        % true .
+
+        % ?- modulo(s(0),s(0),0).
+        % true .
+
+        % ?- modulo(s(s(s(0))),s(s(0)),s(0)).
+        % true .
+
+        % ?- modulo(s(s(s(s(s(0))))),s(s(s(0))),s(s(0))).
+        % true .
+        
+    % Negativ-Tests:
+        % ?- modulo(s(0),s(0),s(0)).
+        % false.
+
+        % ?- modulo(s(s(0)),s(0),s(0)).
+        % false.
