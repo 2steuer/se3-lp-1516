eb5821c03e8d2f93f6e2abbe8be7944139a14568
mcd01 <4scheine@informatik.uni-hamburg.de>
2015-12-04 20:30:40 +0100

    Aufgabe 2 fast fertig
    Rest mache ich morgen! Entspaaaant

--- Uebung-7/praedikate.pl
+++ Uebung-7/praedikate.pl
@@ -20,11 +20,11 @@ get_file_path(VerzNr, DatName, List) :-
 
 %%%%% (1.3)
 
-Die Strategie lautet folgendermaßen:
-- Erstellen einer Liste, die Tupel enthält, in denen das Änderungsdatum einer Datei, der Name, die ID sowie das enthaltende Verzeichnis vermerkt sind.
-- Sortieren dieser Liste nach dem Datum, Absteigenderweise
-- Die ersten N Tupel aus der Liste extrahieren
-- Anhand der zwischengespeicherten ID, Verzeichnis-ID und des Namens Tupel in eine Liste schreiben, welche Datei-ID und Pfad enthalten.
+% Die Strategie lautet folgendermaßen:
+% - Erstellen einer Liste, die Tupel enthält, in denen das Änderungsdatum einer Datei, der Name, die ID sowie das enthaltende Verzeichnis vermerkt sind.
+% - Sortieren dieser Liste nach dem Datum, Absteigenderweise
+% - Die ersten N Tupel aus der Liste extrahieren
+% - Anhand der zwischengespeicherten ID, Verzeichnis-ID und des Namens Tupel in eine Liste schreiben, welche Datei-ID und Pfad enthalten.
 
 % get_n_first_elements(+N, +InList, -OutList).
 % Gibt aus einer Liste der Tupel [Aenderungsdatum, Dateiname, DateiID, VerzeichnisID]
@@ -56,8 +56,73 @@ last_changed_files(N, L) :-
 	reverse(SortedFileList,ReversedList), 		% Hole die letzte Aenderung nach vorn
 	get_n_first_elements(N, ReversedList, L). 	% Und hole die ersten N Eintraege
 
-Der Rechenzeitbedarf ist hier insofern relativ groß, als dass im ersten Schritt alle Dateien in eine große Liste schreiben (O(?)),
-Diese Liste dann sortieren (O(n log n)), und hiernach noch zwei mal durch ie Liste gehen um sie ein mal umzukehren
-und um die ersten N Elemente aus ihr zu extrahieren. (2*O(n)).
-Verbesserungsbedarf wäre hier, wie bereits angesprochen noch in der sort-Funktion, welche im besten Falle die Reverse-Funktion insofern überflüssig macht,
-als dass man direkt das Absteigende Sortieren erlaubt.
+% Der Rechenzeitbedarf ist hier insofern relativ groß, als dass im ersten Schritt alle Dateien in eine große Liste schreiben (O(?)),
+% Diese Liste dann sortieren (O(n log n)), und hiernach noch zwei mal durch die Liste gehen um sie ein mal umzukehren
+% und um die ersten N Elemente aus ihr zu extrahieren. (2*O(n)).
+% Verbesserungsbedarf wäre hier, wie bereits angesprochen noch in der sort-Funktion, welche im besten Falle die Reverse-Funktion insofern überflüssig macht,
+% als dass man direkt das Absteigende Sortieren erlaubt.
+
+%%%%% (2.2)
+
+% list2tree(+Liste,?Baum)
+
+list2tree([ ],end). % Rekursionsabschluss
+% Mit I kann ein String uebergeben werden, der den direkten Vorgaenger in der Liste beschreibt.
+list2tree([[E,I]|R],t(w(E,I),VB,HB)) :-
+compound([E,I]),
+string(I),
+split(R,E,VL,HL),
+list2tree(VL,VB),
+list2tree(HL,HB).
+% Wenn keine Informationen abgespeichert werden sollen, wird dies standardmaessig ausgefuehrt.
+list2tree([E|R],t(E,VB,HB)) :-
+split(R,E,VL,HL),
+list2tree(VL,VB),
+list2tree(HL,HB).
+
+%split(+Liste,+MittleresElement,?VordereElemente,?HintereElemente)
+split([ ],_,[ ],[ ]).
+% angepasst an die Forderung, Informationen abspeichern zu koennen.
+split([[E,I]|R],M,[[E,I]|VL],HL) :-
+compound([E,I]),
+string(I),
+E@=<M, split(R,M,VL,HL).
+split([[E,I]|R],M,VL,[[E,I]|HL]) :-
+compound([E,I]),
+string(I),
+E@>M, split(R,M,VL,HL).
+
+% aus dem Skript, unmodifiziert.
+split([E|R],M,[E|VL],HL) :-
+E@=<M, split(R,M,VL,HL).
+split([E|R],M,VL,[E|HL]) :-
+E@>M, split(R,M,VL,HL).
+
+%%%%% (2.3)
+
+% Aus dem Skript.
+% tree2list(+Baum,?Liste)
+tree2list(end,[ ]).
+tree2list(t(E,VB,HB),L) :-
+tree2list(VB,VL),
+tree2list(HB,HL),
+append(VL,[E|HL],L).
+
+% Ermittelt die an ein Element gebundene Information.
+% Gibt False zurueck, wenn das Element nicht in der Liste enthalten ist, oder zu dem Element keine Informationen existieren.
+% getinfo(+Element,+Baum,?Information)
+getinfo(E,B,I):-
+        tree2list(B,L), % Wandelt Baum in Liste um.
+        information(E,L,I). % Ermittelt die Information.
+% Hilfspraedikat.
+% information(+Element,+Liste,?Information)
+information(E,[w(E,I)|_],I).
+information(_,[],I):- compound(I). % Generiert ein False. Wir wussten nicht, wie man dies sonst implementieren koennte. 
+information(E,[w(X,_)|RL],I):-
+        compound(w(X,_)),
+        E =\= X,
+        information(E,RL,I).
+information(E,[X|RL],I):- % Ueberspringt "normale" Elemente der Liste, also jene, die informationslos sind.
+        not(compound(X)),
+        information(E,RL,I).
+        
--- Uebung-7/steuer_maier_scheinert_07.pl
+++ Uebung-7/steuer_maier_scheinert_07.pl
@@ -34,3 +34,81 @@
 % Ich habe diesen Fehler nicht mehr beheben koennen.
 % Im ersten Anlauf jedoch funktioniert dies. Auch mit großen N (> Dateizahl) wird kein Fehler geworden, sondern
 % der Fall wird korrekt behandelt.
+
+%%%%%%%%%%%%%%%%%%%
+%% Aufgabe 2 %%%%%%
+%%%%%%%%%%%%%%%%%%%
+
+% 1) Das Praedikat macht bei der als Argument uebergebenen Liste das erste Element zur Wurzel des Baums. Alle anderen Elemente
+% werden dann daraufhin untersucht, ob sie groesser oder kleiner als das Wurzelelement sind. Generell gilt: Ist ein Element kleinergleich 
+% einem gegebenen Wurzelelement, wird es zum "linken" Kind der Wurzel. Ist ein Element groesser als das Wurzelelement,wird es zum "rechten" Kind.
+% Beispiel:
+% ?- list2tree([1,0,2],Baum).
+% Baum = t(1, t(0, end, end), t(2, end, end)) 
+% Die 1 wird zum Wurzelelement. Die 0 ist das einzige Element kleinergleich der 1, sie wird also zum linken Kind.
+% Die 2 ist das einzige Element groesser der 1, sie wird also zum rechten Kind. In diesem Fall sind 0 und 1 sogar Blaetter, da sie
+% selbst keine Kinder mehr haben.
+% Dies zeigt auch schon, wie eine Eingabeliste sortiert sein muesste, damit ein daraus entstehender Baum balanciert ist.
+% Zum einen muss die Eingabeliste eine ungerade Laenge haben, denn abzueglich des ersten Elements, was als Wurzel des Baumes dient,
+% muessen gerade viele Elemente vorhanden sein, um einen balancierenden Baum zu erzeugen.
+% Auch muss es von dieser geraden Anzahl an Elementen gleich viele Elemente kleinergleich und groesser des ersten Elements der Liste geben.
+% Eine Liste der Form [2,1,0,3,4] wuerde also zum Erfolg fuehren, nicht aber eine Liste der Form [1,0,2,3].
+
+% 2)
+% Wir haben das Praedikat so angepasst, das grundsaetzlich zwei Operationen notwendig sind.
+% Es ist weiterhin moeglich, wie vorher einfach nur einzelne Elemente in eine Liste einzutragen, und diese
+% Liste dann zu einem Baum zu machen.
+% Auf der anderen Seite ist es aber auch moeglich, zusaetzliche Informationen in Form eines einzelnen Strings abzuspeichern.
+% Dies sieht dann wie folgt aus: [[2,"Wurzel"],[1,"KnotenLinks"],[3,"KnotenRechts"]]. Nach laengerem Ueberlegen kam uns diese
+% Realisierung am sinnvollsten vor. Moechte man also Informationen einem Element der Liste mitgeben, kann man dies mit 
+% [Element, Information] tun. Funktionieren tut das ganze auch:
+
+    % Test, wobei man jedem Element Informationen hinzufuegt.
+    % ?- list2tree([[2,"Wurzel"],[3,"KnotenRechts"],[2,"KnotenLinks"]],Baum).
+    % Baum = t(w(2, "Wurzel"), t(w(2, "KnotenLinks"), end, end), t(w(3, "KnotenRechts"), end, end)) .
+    
+    % Test, wobei man der Haelfte der Elemente Informationen hinzufuegt.
+    % ?- list2tree([[2,"Wurzel"],3,[1,"KnotenLinks"],0,[4,"KnotenRechts"]],Baum).
+    % Baum = t(w(2, "Wurzel"), t(w(1, "KnotenLinks"), t(0, end, end), end), t(3, end, t(w(4, "KnotenRechts"), end, end))) .
+    
+    % Test, ob die alte Funktionalitaet unbeschaedigt ist.
+    Baum = t(2, t(1, t(0, end, end), end), t(3, end, t(4, end, end))) .
+   
+% 3)
+% Idee: Wir benutzen das Praedikat aus dem Skript, mit dem man einen Baum in eine Liste umwandeln kann.
+% In der fertigen Liste suchen wir dann einfach nach dem Key(dem Element) und koennen so die Information,
+% die an das jeweilige Element gebunden ist, abfragen. Dafuer muessen wir noch ein neues Praedikat schreiben.
+% Dieses Praedikat schaut sich jedes Element der Liste an, vergleicht es mit dem Element aus der Anfrage,
+% und gibt bei einer Uebereinstimmung die hinterlegte Information zurueck.
+ 
+    % Test: Funktioniert das Umwandeln, auch von unseren modifizierten Baeumen, in eine Liste?
+    % ?- tree2list(t(w(2, "Wurzel"), t(w(2, "KnotenLinks"), end, end), t(w(3, "KnotenRechts"), end, end)),Liste).
+    % Liste = [w(2, "KnotenLinks"), w(2, "Wurzel"), w(3, "KnotenRechts")].
+    % Test ist geglueckt.
+    
+    % Test: Arbeitet das definierte Praedikat information so, wie erwartet?
+    
+    % ?- information(3,[w(2, "Wurzel"), w(3, "KnotenRechts")],Information).
+    % Information = "KnotenRechts" .
+    % True fuer Elemente, die in der Liste enthalten sind und Informationen besitzen.
+    
+    % ?- information(5,[w(2, "Wurzel"), w(3, "KnotenRechts")],Information).
+    % false.
+    % False fuer Elemente, die zwar Informationen besitzen, aber nicht in der Liste sind.
+    
+    % ?- information(4,[w(2, "Wurzel"),4,5,6],Information).
+    % false.
+    % False fuer Elemente, die zwar in der Liste sind, aber keine Informationen besitzen.
+    
+    % Test des Gesamtpraedikats getinfo, welches als Argumente ein Element und einen Baum nimmt,
+    % und die entsprechende Information, wenn existent, zurueckgibt.
+    
+    % ?- getinfo(3,t(w(2, "Wurzel"), t(w(2, "KnotenLinks"), end, end), t(w(3, "KnotenRechts"), end, end)),Information).
+    % Information = "KnotenRechts" .
+    
+    % ?- getinfo(3,t(w(2, "Wurzel"), t(w(2, "KnotenLinks"), end, end),end),Information).
+    % false.
+    
+    % ?- getinfo(3,t(w(2, "Wurzel"), t(w(2, "KnotenLinks"), end, end), t(3, end, end)),Information).
+    % false.
+    
