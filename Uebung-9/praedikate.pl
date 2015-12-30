da6ac89192e3996af1dfbdb895f73e3567ed4f5b
mcd01 <4scheine@informatik.uni-hamburg.de>
2015-12-30 20:19:53 +0100

    Ausgabe jetzt im Stringformat,

--- Uebung-9/praedikate.pl
+++ Uebung-9/praedikate.pl
@@ -1,16 +1,23 @@
 dynamic rule/2.
 % rule(+Pattern,+Response)
 
-rule([[ich,habe],Menge,Art,[gegessen]],[[warum,hast,du],Menge,Art,[gegessen,'?']]).						    					  
+rule([[ich,fuehle,mich],Gefuehl,[heute]],[[warum,fuehlst,du,dich],Gefuehl,['?']]).	
+rule([[ich,habe],Menge,Art,[gegessen]],[[warum,hast,du],Menge,Art,[gegessen,'?']]).					    					  
                       
+					  
+					  
+
 % Eine Liste, die ueber die Konsole eingegeben wird, ruft das folgende Praedikat auf.	 
 [X]:- workit(X).
 [X1|X]:- workit([X1|X]).	 
 workit(X):- rule(Y,_), 
 			checkpattern(Y,X,[],Y2),    % baut das instanziierte Pattern
 		    reverse(Y2,Y3),          
-			rule(Y3,Z),                 % Holt das zugehoerige Response
-			write(Z),!.                 % Schreibt das Response auf die Konsole
+			rule(Y3,Z), 				% Holt das zugehoerige Response
+		    reducelist(Z,[],Z1),
+			reverse(Z1,Z2),
+			stringlist(Z3,Z2),
+			write(Z3),!.                 % Schreibt das Response auf die Konsole im Stringformat.
 			% Durch den Cut beenden wir die Suche nach der ersten erfolgreich angewandten Regel
 
 % checkpattern /4.
@@ -40,7 +47,15 @@ checkpattern([[]|A],B,C,D):- checkpattern(A,B,C,D).
 checkpattern([A1|A],[B1|B],C,D):- A1 \== B1, 
 								  checkpattern([A1|A],B,C,D).
 % Fuer das Backtracking.								  
-checkpattern([A1|A],[_|B],C,D):- checkpattern([A1|A],B,C,D).							  
-								 						  
+checkpattern([A1|A],[_|B],C,D):- checkpattern([A1|A],B,C,D).	
 
+% Wandelt eine Liste in einen String.
+stringlist(String,List):- atomic_list_concat(List,' ',Atom),
+						  atom_string(Atom,String).						 			  
 
+% Reduziert eventuell verschachtelte Listen zu einer großen.
+reducelist([],A,A).
+reducelist([[]|A],B,C):- reducelist(A,B,C).
+reducelist([[A1|A2]|A],B,C):- reducelist([A2|A],[A1|B],C).
+reducelist([A1|A],B,C):- not(is_list(A1)),
+						 reducelist(A,[A1|B],C).						 
